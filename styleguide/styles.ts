26431420972bf8cbac106466384021806ca400a2
Ugo Palatucci <upalatuc@redhat.com>
2022-02-03 15:41:08 +0100

    Doc style fix 2 (#14)

--- styleguide/styles.ts
+++ styleguide/styles.ts
@@ -1,21 +1,23 @@
 import { Theme } from 'react-styleguidist/lib/typings/RsgTheme';
 
-module.exports = function styles(theme: Theme) {
-  return {
-    Playground: {
-      preview: {
-        paddingLeft: 0,
-        paddingRight: 0,
-        borderWidth: [[0, 0, 1, 0]],
-        borderRadius: 0,
+module.exports = {
+  styles: (theme: Theme) => {
+    return {
+      Playground: {
+        preview: {
+          paddingLeft: 0,
+          paddingRight: 0,
+          borderWidth: [[0, 0, 1, 0]],
+          borderRadius: 0,
+        },
       },
-    },
-    Code: {
-      code: {
-        // make inline code example appear the same color as links
-        color: theme.color.link,
-        fontSize: 14,
+      Code: {
+        code: {
+          // make inline code example appear the same color as links
+          color: theme.color.link,
+          fontSize: 14,
+        },
       },
-    },
-  };
+    };
+  },
 };
