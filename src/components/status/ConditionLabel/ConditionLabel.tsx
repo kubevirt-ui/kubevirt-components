ebe5932154fc11f01ae2e73b06578079bcf16dc0
Ugo Palatucci <upalatuc@redhat.com>
2022-02-15 17:49:59 +0100

    condition label doc

--- src/components/status/ConditionLabel/ConditionLabel.tsx
+++ src/components/status/ConditionLabel/ConditionLabel.tsx
@@ -3,14 +3,29 @@ import * as React from 'react';
 import { Label, Popover, PopoverPosition } from '@patternfly/react-core';
 
 export interface k8sStatusConditions {
+  /**
+   * Type indicate which type of condition is.
+   */
   type: string;
+  /**
+   * Status is the high level overview of how the resource is doing. It contains information available to controllers and users.
+   */
   status?: string;
+  /**
+   * A machine-readable description of the cause of the error. If this value is empty there is no information available.
+   */
   reason?: string;
+  /**
+   * A human-readable description of the cause of the error. This field may be presented as-is to a reader.
+   */
   message?: string;
 }
 
 /**
- * ConditionLabel renders a k8sStatusConditions
+ * ConditionLabel renders a k8sStatusConditions.
+ * Usefull to give information of what the resouce is doing without taking up much space into the interface.
+ *
+ * On mouse click it creates a popover to explain the state in a more disursive way.
  */
 export const ConditionLabel: React.FC<k8sStatusConditions> = React.memo((condition) => {
   const preventLabelLink = React.useCallback((e) => e.preventDefault(), []);
--- src/components/status/ConditionLabelList/ConditionLabelList.tsx
+++ src/components/status/ConditionLabelList/ConditionLabelList.tsx
@@ -2,15 +2,22 @@ import * as React from 'react';
 
 import { LabelGroup } from '@patternfly/react-core';
 
-import { ConditionLabel, k8sStatusConditions } from '../ConditionLabel';
+import { ConditionLabel } from '../ConditionLabel';
+import { k8sStatusConditions } from '../ConditionLabel/ConditionLabel';
 
-/**
- * VirtualMachineCondition renders a list of a k8s resource conditions
- * */
 export interface ConditionLabelListProps {
+  /**
+   * Array of k8s status conditions.
+   * k8s status conditions are similar to [KubeVirt Condition](https://kubevirt.io/api-reference/master/definitions.html#_v1_kubevirtcondition) but can represent also different kind of conditions
+   */
   conditions: k8sStatusConditions[];
 }
 
+/**
+ * ConditionLabelList renders a list of a k8s resource conditions.
+ *
+ * This component can me usefull to give an in dept clues of what the resourse is doing.
+ * */
 export const ConditionLabelList: React.FC<ConditionLabelListProps> = React.memo(
   ({ conditions }) => {
     return (
